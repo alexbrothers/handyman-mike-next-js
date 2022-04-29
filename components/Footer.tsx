8e0321c4c72df5ef8d239e71009263e5597dd392
alexbrothers <alexander.b.brothers@gmail.com>
2022-04-28 22:22:00 -0400

    Update typo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
                     <Typography paragraph textAlign="center" sx={{
                         marginBottom: 0
                     }}>
-                        Copywright © 2022 Handyman Mike, LLC
+                        Copyright © 2022 Handyman Mike, LLC
                     </Typography>
                     <Box sx={{
                         display: "flex",
